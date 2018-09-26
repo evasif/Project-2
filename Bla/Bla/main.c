0319a367bd90bfd65df33d8dafd0868e84f38f7f
evasif <evasiif@gmail.com>
2018-09-26 17:02:17 +0000

    Checking for available ports

---
+++ Bla/Bla/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Bla
+//
+//  Created by Eva Sif on 26/09/2018.
+//  Copyright © 2018 Eva Sif. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
--- Project2/Project2/server.cpp
+++ Project2/Project2/server.cpp
@@ -7,6 +7,11 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
+#include <iostream>
+#include <netdb.h>
+#include <vector>
+
+using namespace std;
 
 
 // 1. create a socket - Get the file descriptor!
@@ -17,6 +22,75 @@
 // 6. shutdown to end read/write.
 // 7. close to releases data.
 
+vector<int> check_open_ports () {
+    int port = 4000;
+    int count = 1;
+
+    // Creating variables
+    struct sockaddr_in server_addr;
+    int sock_fd, connected;
+    struct hostent *server;
+
+
+    while(count < 4) {
+        // Create the TCP socket
+        sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+        // Error check for the socket
+        if (sock_fd < 0) {
+            perror("socket() error");
+            exit(-1);
+        }
+
+        // Initialise the socketaddr_in structure
+        server_addr.sin_family = AF_INET;
+
+        // Fill in the port number
+        server_addr.sin_port = htons(port);
+
+        // Getting the descriptor for the corresponding IP address
+        server = gethostbyname("127.0.0.1");
+
+        // Error check for the host
+        if (server == NULL) {
+            perror("host() error");
+            exit(0);
+        }
+
+        // Doing a memory copy to put it into the right format (right field in the structure)
+        memcpy((char *)&server_addr.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
+
+        // Connecting to a remote address
+        connected = connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+        // Checking if this specific port is open or closed
+        if (connected < 0) {
+            cout << "port " << port << " is closed" << endl;
+            count++;
+            port++;
+
+            close(sock_fd);
+        } else {
+            cout << "port " << port << " is open" << endl;
+            count = 0;
+            port++;
+            close(sock_fd);
+        }
+    }
+
+    vector<int> ports;
+
+    for(int i = 1; i < 4; i++) {
+        ports.push_back(port-2);
+        port++;
+    }
+
+    return ports;
+}
+
+
+
+
 int main(int argc, char *argv[])
 {
     // Creating variables
@@ -26,25 +100,34 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr, cli_addr;
     int n;
     int max_sd;
-    int arr[] = { 5000, 5001, 5002 };
+    vector<int> ports = check_open_ports();
+    cout << "Hello1" << endl;
+
     //set of socket descriptors
     fd_set readfds;
 
+    cout << "SIZE " <<  ports.size() << endl;
+
+    for (int i = 0; i <  ports.size(); i++) {
+        cout << i << " " << ports[i] << endl;
+    }
+
+
     /*if (argc < 2) {
-        perror("No port provided");
-        exit(-1);
-    }*/
+     perror("No port provided");
+     exit(-1);
+     }*/
     /*while(TRUE) {
-        //clear the socket set
-        FD_ZERO(&readfds);
+     //clear the socket set
+     FD_ZERO(&readfds);
 
-        //add master socket to set
-        FD_SET(sock_fd, &readfds);
-        max_sd = sock_fd;
+     //add master socket to set
+     FD_SET(sock_fd, &readfds);
+     max_sd = sock_fd;
 
-    }*/
+     }*/
 
-    for (int i = 0; i < sizeof(arr); i++) {
+    for (int i = 0; i <  ports.size(); i++) {
         // Create the socket
         sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
@@ -58,7 +141,7 @@ int main(int argc, char *argv[])
         bzero((char *) &serv_addr, sizeof(serv_addr));
 
         // Setting the port number to the provided argument
-        portno = arr[i];
+        portno = ports[i];
 
         // Initialise the socketaddr_in structure
         serv_addr.sin_family = AF_INET;
@@ -79,43 +162,49 @@ int main(int argc, char *argv[])
         }
         // This listen() call tells the socket to listen to the incoming connections and places all incoming
         // connection into a backlog queue until accept() call accepts the connection
-        // Here the maximum size for the backlog queue is 5
-        listen(sock_fd,5);
+        // Here the maximum size for the backlog queue is 3
+        listen(sock_fd, 3);
 
         // Accepts an incoming connection
         clilen = sizeof(cli_addr);
+
+
+        // This accept() function will write the connecting client's address info
+        // into the the address structure and the size of that structure is clilen.
+        // The accept() returns a new socket file descriptor for the accepted connection.
+        // So, the original socket file descriptor can continue to be used
+        // for accepting new connections while the new socker file descriptor is used for
+        // communicating with the connected client.
+        new_sock_fd = accept(sock_fd, (struct sockaddr *) &cli_addr, &clilen);
+
+        if (new_sock_fd < 0) {
+            perror("Error on accept");
+            exit(-1);
+        }
+
+        printf("server: got connection from %s port %d\n", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
+
+
+        // This send() function sends the 13 bytes of the string to the new socket
+        send(new_sock_fd, "Hello, world!\n", 13, 0);
+
+        bzero(buffer, 256);
+
+        n = read(new_sock_fd, buffer, 255);
+
+        if (n < 0) {
+            perror("Error reading from socket");
+            exit(-1);
+        }
+        printf("Here is the message: %s\n", buffer);
     }
 
-    // This accept() function will write the connecting client's address info
-    // into the the address structure and the size of that structure is clilen.
-    // The accept() returns a new socket file descriptor for the accepted connection.
-    // So, the original socket file descriptor can continue to be used
-    // for accepting new connections while the new socker file descriptor is used for
-    // communicating with the connected client.
-    new_sock_fd = accept(sock_fd, (struct sockaddr *) &cli_addr, &clilen);
-
-    if (new_sock_fd < 0) {
-        perror("Error on accept");
-        exit(-1);
-    }
-
-    printf("server: got connection from %s port %d\n", inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port));
-
-
-    // This send() function sends the 13 bytes of the string to the new socket
-    send(new_sock_fd, "Hello, world!\n", 13, 0);
-
-    bzero(buffer,256);
-
-    n = read(new_sock_fd,buffer,255);
-
-    if (n < 0) {
-        perror("Error reading from socket");
-        exit(-1);
-    }
-    printf("Here is the message: %s\n",buffer);
-    
     close(new_sock_fd);
     close(sock_fd);
     return 0;
 }
+
+
+/* They should be allocated dynamically, since part of the assignment is choosing a port range that isn't already in use.
+ * The client can then be told the port range that the server is on, and the sequence, and uses that to connect.
+ * try and connect() to the port. For the subsequent two ports, don´t forget to close() them if they´re open... */
